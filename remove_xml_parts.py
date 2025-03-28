0066582d368dbb908bfb7ee0b559d604455b240c
kubix23 <kubix2770@gmail.com>
2025-03-28 23:39:25 +0100

    Adding filters to xml and parallelizing operations

--- remove_xml_parts.py
+++ remove_xml_parts.py
@@ -3,13 +3,25 @@
 import os
 import re
 import sys
+import time
 import urllib.request as URL
 import pandas as pd
 import IPython.display as display
 import subprocess
+from PyPDF2 import PdfReader, PdfWriter
+from concurrent.futures import ThreadPoolExecutor
 
 
-def remove_xml_parts(file_path, i):
+def is_pdf_valid(file_path):
+    try:
+        with open(file_path, 'rb') as f:
+            reader = PdfReader(f)
+            _ = reader.pages[0]
+        return True
+    except Exception as e:
+        return False
+
+def remove_xml_parts(file_path):
     """Remove XML parts from the given XML file."""
 
     # save title of file
@@ -17,7 +29,10 @@ def remove_xml_parts(file_path, i):
         content = file.read()
 
     # remove all citations and references in <ref> tags (and whitespaces in front of them)
-    content = re.sub(r'\s*<ref[^>]*>.*?</ref>', '', content, flags=re.S)
+    content = re.sub(r'<ref[^>]*/>', '', content, flags=re.S)
+    content = re.sub(r'<ref[^>]*>.*?</ref>', '', content, flags=re.S)
+    content = re.sub(r'<div[^>]*><head>Spis .*?/div>', '', content, flags=re.S)
+    content = re.sub(r'<p[^>]*>Tabela.*?/div>', '</div>>', content, flags=re.S)
 
     # Step 1: Extract only the <p> content
     p_matches = re.findall(r'<p>(.*?)</p>', content, re.S)
@@ -31,8 +46,36 @@ def remove_xml_parts(file_path, i):
     # Remove references to bibliography eg. "[35]"
     clean_text = re.sub(r'\[\d+\]', '', final_text)
     clean_text = re.sub(r'\s+', ' ', clean_text).strip()
-
     return clean_text
+
+
+def doctorate_execute(doc, i):
+    print("Processing document ", i + 1, "/", len(doc))
+    title = doc["Title"][i].replace(" ", "_").replace('./', '').replace('/', '_')
+    try:
+        os.makedirs(f"doct/{title}")
+    except FileExistsError:
+        pass
+
+    for l in range(5):
+        if is_pdf_valid(f"doct/{title}/doc.pdf") and os.path.exists(f"doct/{title}/doc.pdf"):
+            break
+        try:
+            URL.urlretrieve(doc["File"][i], filename=f"doct/{title}/doc.pdf")
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(1 * l)
+
+    try:
+        subprocess.run(
+            [sys.executable, "-m", "grobid_client.grobid_client", "--input", f".\\doct\\{title}",
+             "processFulltextDocument"],
+            cwd=os.getcwd())
+        print(f"Processing {title}.grobid.tei.xml")
+        doc.at[i, "Text"] = remove_xml_parts(f"doct/{title}/doc.grobid.tei.xml")
+    except Exception as e:
+        print(e)
 
 
 # Usage example
@@ -40,28 +83,10 @@ if __name__ == "__main__":
     df_doc = pd.read_csv("./Smaller_Files/doctorates_3.csv")
 
     display.display(df_doc)
+    doctorate_count = len(df_doc)
 
-    for i in range(len(df_doc)):
-        print("Processing document ", i+1, "/", len(df_doc))
-        try:
-            title = df_doc["Title"][i].replace(" ", "_").replace('./', '')
-            URL.urlretrieve(df_doc["File"][i], filename=f"{title}.pdf")
-            #subprocess.run(["python3", "-m", "grobid_client.grobid_client", "--input", "./", "processFulltextDocument"])
-            subprocess.run(
-                [sys.executable, "-m", "grobid_client.grobid_client", "--input", "./", "processFulltextDocument"],
-                cwd=os.getcwd())
-            print(f"Processing {title}.grobid.tei.xml")
-            df_doc.at[i, "Text"] = remove_xml_parts(f"{title}.grobid.tei.xml", i)
-            # with open(f"{title}.txt", "w", encoding="utf-8") as file:
-            #     file.write(df_doc.at[i, "Text"])
-            os.remove(f"{title}.grobid.tei.xml")
-        except:
-            print(f"Error processing {title}.grobid.tei.xml")
-        try:
-            os.remove(f"{title}.pdf")
-        except:
-            print(f"Error removing {title}.pdf")
-            continue
+    with ThreadPoolExecutor() as executor:
+        executor.map(lambda i: doctorate_execute(df_doc, i), range(doctorate_count))
 
     df_doc = df_doc.dropna(subset=["Text"])
-    df_doc.to_csv("doctorates_with_text_3.csv", sep='|', index=False)
+    df_doc.to_csv("doctorates_with_text_6.csv", sep='|', index=False)
