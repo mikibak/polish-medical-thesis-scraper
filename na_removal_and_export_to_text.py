800f2369629ba0a87ad056a6eea597fdd40f1a43
Jakub Kondracki <s188767@student.pg.edu.pl>
2025-03-30 19:17:49 +0200

    Formatting files and giving proper naming

---
+++ na_removal_and_export_to_text.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Load the doctorates.csv file
+df = pd.read_csv('./doctorates_with_text_2.csv', sep='|')
+
+## Remove rows with missing text
+df = df.dropna(subset=['Text'])
+
+#saving each df['Text'] in a txt file not based on index
+for i in range(len(df)):
+    with open(f"./text_files/doctorates_2_doc{i}.txt", "w", encoding="utf-8") as file:
+        file.write(str(df['Text'][i]))
+
+df.drop(columns=['Text'], inplace=True)
+df.to_csv('./doctorates_without_na/doctorates_links_2.csv', sep='|', index=False)
+
